da2134285969c2b6aa1d190e9547e6a621234c6a
eviltik <eviltik@gmail.com>
2018-03-04 11:49:50 +0100

    update tests

--- test/download_error_invalid_subscriptionKey.js
+++ test/download_error_invalid_subscriptionKey.js
@@ -2,16 +2,15 @@ const tap = require('tap');
 
 let options = {
     subscriptionKey:'invalidKey-test-udger-updater-nodejs',
-    currentDatabase:__dirname+'/db/udgerdb_v3_test.dat',
-    nextDatabase:'./tmp.dat'
+    currentDatabase:__dirname+'/db/udgerdb_v3_test.dat'
 };
 
 tap.test(
-    'invalid key should return 404',
+    'invalid key should return bad api key',
     (t) => {
         const udgerUpdater = require('../')(options);
         udgerUpdater.downloadNow((err) => {
-            t.equal(err.message.match(/404/)[0], '404');
+            t.equal(err.message.match(/bad api key/)[0], 'bad api key');
             t.end();
         });
     }
